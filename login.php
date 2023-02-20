f0d18dc9294c7ae61692f8c9cea8346eb2786fe9
abhhi <birdawade11@gmail.com>
2023-02-20 20:23:22 +0530

    Update and rename login.php to index.html

--- login.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta http-equiv="refresh" content="0; URL=http://test.biome.app/login">
-  </head>
-  <body>
-    <p>Please wait while you are redirected to example.com...</p>
-  </body>
-</html>
