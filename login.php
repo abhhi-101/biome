0013781db990d8007805cc8d203b83a91799040b
abhhi <birdawade11@gmail.com>
2023-02-20 20:16:00 +0530

    Update and rename login.html to login.php

---
+++ login.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta http-equiv="refresh" content="0; URL=http://test.biome.app/login">
+  </head>
+  <body>
+    <p>Please wait while you are redirected to example.com...</p>
+  </body>
+</html>
